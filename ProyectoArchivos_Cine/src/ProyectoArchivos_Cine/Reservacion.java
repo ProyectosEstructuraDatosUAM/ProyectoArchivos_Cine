a24856afc9e71ba1c781400efac7757dd5ef5190
DeathCom <www-rainbow_tables@outlook.com>
2017-10-02 22:27:12 -0600

    Last Commit
    Se logra subir todos los cambios realizados

---
+++ ProyectoArchivos_Cine/src/ProyectoArchivos_Cine/Reservacion.java
@@ -0,0 +1,79 @@
+
+package ProyectoArchivos_Cine;
+/**
+ *
+ * @author infinitus
+ */
+public class Reservacion {
+    private String nombre_completo;
+    private String correo_electronico;
+    private String cedula;
+    private String telefono;
+    private String Cantidad_tiquetes;
+    private String tipo_tiquete;
+    private String pelicula;
+
+    public String getNombre_completo() {
+        return nombre_completo;
+    }
+
+    public void setNombre_completo(String nombre_completo) {
+        this.nombre_completo = nombre_completo;
+    }
+
+    public String getCorreo_electronico() {
+        return correo_electronico;
+    }
+
+    public void setCorreo_electronico(String correo_electronico) {
+        this.correo_electronico = correo_electronico;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCantidad_tiquetes() {
+        return Cantidad_tiquetes;
+    }
+
+    public void setCantidad_tiquetes(String Cantidad_tiquetes) {
+        this.Cantidad_tiquetes = Cantidad_tiquetes;
+    }
+
+    public String getTipo_tiquete() {
+        return tipo_tiquete;
+    }
+
+    public void setTipo_tiquete(String tipo_tiquete) {
+        this.tipo_tiquete = tipo_tiquete;
+    }
+
+    public String getPelicula() {
+        return pelicula;
+    }
+
+    public void setPelicula(String pelicula) {
+        this.pelicula = pelicula;
+    }
+
+
+
+    
+
+    
+    
+    
+}
